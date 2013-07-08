9fc4f7d5cd32054469e2d4910be4176e2669d3b6
Railwaymen <railwaymen@railwaymen.org>
2013-07-08 19:27:30 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AskExpert::Application.config.secret_key_base = '0112caf6f21004749340ac39fa7d7a01d97c211ab473a52c1d2c11965bba7b221f11b2ba1fb7fa1bd0576bb25fac8a0da8d7eacfc911925b06be7325c76d6926'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AskExpert::Application.config.session_store :cookie_store, key: '_ask_expert_session'
