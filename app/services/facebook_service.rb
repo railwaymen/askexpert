8b208a6979215548392aa49a869c952a18a49af7
Railwaymen <railwaymen@railwaymen.org>
2013-07-14 12:40:56 +0200

    Fix fb service

--- app/services/facebook_service.rb
+++ app/services/facebook_service.rb
@@ -1,5 +1,5 @@
 class FacebookService
   PROVIDER = "facebook"
   SCOPES = "publish_stream, email, user_about_me, user_location, user_interests, user_activities, user_likes, friends_about_me, friends_location, friends_interests, friends_activities, friends_likes"
-  FIELDS = ["id", "email", "name", "bio" "location", "interests", "activities", "languages"]
+  FIELDS = "id, email, name, bio, location, interests, activities, languages"
 end
